902c297c70e1f063441d0c24c4019e48c3f6232e
x1hntr <x1dxniel@gmail.com>
2022-12-17 20:13:32 -0500

    firstCommit

---
+++ src/TabataTimer.jsx
@@ -0,0 +1,7 @@
+
+
+export const TabataTimer = () => {
+  return (
+    <div className="bar-title" >TABATA TIMER</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css';
+import { TabataTimer } from './TabataTimer'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TabataTimer />
+  </React.StrictMode>,
+)
